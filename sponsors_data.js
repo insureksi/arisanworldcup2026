fb7398e184564592359960ed52505601fea4544e
insureksi <insureksi@gmail.com>
2026-06-04 10:42:29 +0700

    Add files via upload

--- sponsors_data.js
+++ sponsors_data.js
@@ -1,1 +1,1 @@
-﻿const DAFTAR_SPONSOR = ["cp-company-logo.png","fnf-logo.jpg","images.jpg","logo-si.png","ma-strum-logo.jpg","moundfield-logo.webp"];
+﻿const DAFTAR_SPONSOR = ["cp-company-logo.png","images.jpg","logo-si.png","ma-strum-logo.jpg"];
